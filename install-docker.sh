e8507344760f355213fcdb0f861e8d9cbeed3b2a
Jayson Ryter <jaysonryter@ryterinc.com>
2019-06-20 16:36:19 -0400

    syntax error

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/basha
+#!/bin/bash
 
 # uninstall docker
 yum remove docker \
@@ -24,8 +24,6 @@ yum-config-manager \
 yum install -y docker-ce docker-ce-cli containerd.io
 
 # Install docker-compose
-echo "Installing Docker-compose"
-
 curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
